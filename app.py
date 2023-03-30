5f7d105fef3e07eb8b1e7eb912820bc689d33d2d
junvely <junvely97@gmail.com>
2023-03-30 23:18:14 +0900

    파일 정리

--- app.py
+++ app.py
@@ -61,28 +61,6 @@ def add_user():
    return jsonify({'msg': '회원가입이 완료되었습니다'})
 
 
-# # 게시글 작성 완료시 게시글을 feed table에 저장하는 POST요청
-# @app.route("/addfeed", methods=["POST"])
-# def add_feed():
-#    nickname_receive = request.form['nickname_give']
-#    title_receive = request.form['title_give']
-#    description_receive = request.form['description_give']
-#    date_receive = request.form['date_give']
-#    like_receive = request.form['like_give']
-
-#    doc = {
-#       'nickname': nickname_receive,
-#       'title': title_receive,
-#       'description': description_receive,
-#       'date': date_receive,
-#       'like': like_receive
-#     }
-   
-#    db.feed.insert_one(doc)
-#    return jsonify({'msg': '피드 작성 완료!'})
-
-
-
 # 유저 정보 가져오는 GET요청
 @app.route("/userget", methods=["GET"])
 def user_get():
@@ -149,24 +127,6 @@ def login_post():
             return jsonify({'result': True, 'token': token})
     
     else : return {"result": False}, 404
-
-
-#대식님
-# @app.route("/adduser", methods=["POST"])
-# def add_user():
-#    id_receive = request.form['id_give']
-#    pw_receive = request.form['pw_give']
-#    name_receive = request.form['name_give']
-#    nickname_receive = request.form['nickname_give']
-
-#    doc = {
-#       'user_id': id_receive,
-#       'password': pw_receive,
-#       'name': name_receive,
-#       'nickname': nickname_receive
-#     }
-#    db.user.insert_one(doc)
-#    return jsonify({'msg': '회원가입이 완료되었습니다'})
 
 #한빛님
 @app.route('/test', methods=['POST'])
