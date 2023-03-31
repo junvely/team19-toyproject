5010cb8a727ad960953580e7ee70862141448d22
junvely <junvely97@gmail.com>
2023-03-31 22:21:34 +0900

    내 피드 기능 수정

--- static/js/myfeed.js
+++ static/js/myfeed.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   myfeed_get();
 });
 
-
 function myfeed_get() {
   let tokens = [];
   $("#cards").empty();
@@ -13,7 +12,7 @@ function myfeed_get() {
       for (let i = 0; i < token.length; i++) {
         tokens.push(token[i].Access_token);
       }
-      console.log(token)
+      console.log(token);
       if (tokens.includes(sessionStorage.token)) {
         let formData = new FormData();
         formData.append("token_give", sessionStorage.token);
@@ -33,33 +32,37 @@ function myfeed_get() {
         // Promise.all을 사용하여 usernickname과 feeds를 한 번에 받아옴
         Promise.all([usernicknamePromise, feedsPromise]).then(
           ([usernickname, feeds]) => {
-            console.log(usernickname, feeds)
-            let arr = []
+            console.log(usernickname, feeds);
+            let arr = [];
             for (let i = 0; i < feeds.length; i++) {
               if (usernickname === feeds[i].nickname) {
-                arr.push(feeds[i])
+                arr.push(feeds[i]);
               }
             }
-            console.log(arr[0])
-            for (let j = 0; j < arr; j++) {
+            for (let j = 0; j < arr.length; j++) {
               let temp_feeds_html = `
                 <div class="card" id="card-${j}">
-                  <div class="card-img"><img src="${arr[j].url}" width=100%, height=100%/></div>
+                  <div class="card-img"><img src="${
+                    arr[j].url
+                  }" width=100%, height=100%/></div>
                   <div class="card-title">${arr[j].title}</div>
                   <p class="card-body">${arr[j].description}</p>
                   <div class="like-wrapper">
-                  <div class="like-count"><span><span class="like">♥️</span><span id="like-count-${i}">${arr[j].like}</span></div>
+                  <div class="like-count"><span><span class="like">♥️</span><span id="like-count-${j}">${
+                arr[j].like
+              }</span></div>
                   <div class="like-btn-wrapper"><button id="like-btn-${j}" class="like-btn" data-card-id="${j}">좋아요♥️</button></div>
                   </div>
                   <div class='card-footer'>
                   <p>${arr[j].nickname}</p>
-                  <p>${arr[j].date.split(".")[j]}월 ${arr[j].date.split(".")[2]}일</p>
+                  <p>${arr[j].date.split(".")[j]}월 ${
+                arr[j].date.split(".")[2]
+              }일</p>
                     </div>
                 </div>
               `;
               $("#cards").append(temp_feeds_html);
             }
-
           }
         );
       }
